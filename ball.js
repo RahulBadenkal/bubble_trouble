3b0f8a72a89414474b6600b694f638f38b4f97a4
rahul <rahulbadenkal@gmail.com>
2020-04-03 21:55:22 +0530

    Added velocity, d, u to class variables

--- ball.js
+++ ball.js
@@ -4,30 +4,29 @@ class Ball {
         this.x = x
         this.y = y
         this.radius = radius
+        this.velocity = 2
+        this.d = 1
+        this.u = 1
     }
 
     Draw() {
-        let velocity = 2
-        let d = 1
-        let u = 1
-        console.log(this.y, height)
 
         if (this.x >= (width - this.radius) || (this.x <= this.radius)) {
-            d = d * -1
+            this.d = this.d * -1
         }
-        this.x = this.x + 4 * d;
+        this.x = this.x + 4 * this.d
         
-        if (this.y >= (height - this.radius) || this.y <= this.radius || velocity <= 0) {
-            u = u * -1
+        if (this.y >= (height - this.radius) || this.y <= this.radius || this.velocity <= 0) {
+            this.u = this.u * -1
         }
 
         if (this.y >= height - this.radius) {
-            velocity = 30;
+            this.velocity = 30
         }
 
-        velocity = velocity + u * 3 * 0.5;
-        this.y = this.y + u * velocity;
-        circle(this.x, this.y, this.radius);
+        this.velocity = this.velocity + this.u * 3 * 0.5;
+        this.y = this.y + this.u * this.velocity;
+        circle(this.x, this.y, this.radius)
     }
 
 }
--- sketch.js
+++ sketch.js
@@ -3,14 +3,14 @@
 let b = new Ball(40, 40, 30)
 
 function setup() {
-  createCanvas(1366, 400);  // Size must be the first statement
-  stroke(255);     // Set line drawing color to white
-  frameRate(30);
+  createCanvas(1366, 400)  // Size must be the first statement
+  stroke(255)    // Set line drawing color to white
+  frameRate(30)
 }
 
 function draw() {
-  background(0);
-  b.Draw();
+  background(0)
+  b.Draw()
 }
 
 

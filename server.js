a42e387190d2e461f6837845b2b69dce5f33d358
rahul <rahul@sarci.in>
2020-04-27 21:31:49 +0530

    Added package.json

--- server.js
+++ server.js
@@ -5,11 +5,8 @@ var path = require('path');
 let app = express()
 let server = app.listen(3000)
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.use(express.static(__dirname));
 
-// app.use(express.static('./'))
 
 let io = socket(server)
 io.sockets.on('connection', newConnection)
